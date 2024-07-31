d726a46f3d5f50b552fbb83d12c2ad828a6f6673
IngDavidTM <david-1011@hotmail.com>
2024-07-31 14:59:02 -0500

    Get api information

--- src/components/survey/UserExperiences.vue
+++ src/components/survey/UserExperiences.vue
@@ -3,7 +3,7 @@
     <base-card>
       <h2>Submitted Experiences</h2>
       <div>
-        <base-button>Load Submitted Experiences</base-button>
+        <base-button @click="loadResults" >Load Submitted Experiences</base-button>
       </div>
       <ul>
         <survey-result
@@ -23,6 +23,33 @@ import SurveyResult from './SurveyResult.vue';
 export default {
   components: {
     SurveyResult,
+  },
+  data() {
+    return {
+      results: [],
+    };
+  },
+  methods: {
+    loadResults() {
+      fetch('https://vue-http-demo-d9874-default-rtdb.firebaseio.com/surveys.json')
+        .then((response) => {
+          if (response.ok) {
+            return response.json();
+          }
+        })
+        .then((data) => {
+          const results = [];
+          for (const key in data) {
+            results.push({
+              id: key,
+              name: data[key].name,
+              rating: data[key].rating,
+            });
+          }
+          console.log(results);
+          this.results = results;
+        });
+    },
   },
 };
 </script>
